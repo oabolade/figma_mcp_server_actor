7c72f0e8ff7761ded518307a93b6d7ab59e3e0f7
Olasile Abolade <oabolade@Olasiles-MacBook-Air.local>
2025-11-18 18:08:13 -0800

    Add ESLint v9 flat config file
    ESLint v9 requires eslint.config.js instead of .eslintrc.* format.
    Configured with @apify/eslint-config and eslint-config-prettier.

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import prettierConfig from 'eslint-config-prettier';
+import apifyConfig from '@apify/eslint-config';
+
+const config = [
+    ...apifyConfig,
+    prettierConfig,
+    {
+        files: ['**/*.js', '**/*.mjs'],
+        ignores: ['node_modules/**', 'storage/**', 'dist/**', 'build/**'],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+        },
+    },
+    {
+        files: ['eslint.config.js'],
+        rules: {
+            'import/no-default-export': 'off',
+        },
+    },
+];
+
+export default config;
+
